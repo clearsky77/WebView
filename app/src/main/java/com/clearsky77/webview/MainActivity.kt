da2af1cc6f9cf3922a1b4319ea5e1024c8852ce2
clearsky77 <clearsky3264@gmail.com>
2021-10-24 18:09:09 +0900

    웹뷰 열기위한 준비 작업 1.자바 스크립트 허용 2.새 브라우저에서 열지 않도록 작업

--- app/src/main/java/com/clearsky77/webview/MainActivity.kt
+++ app/src/main/java/com/clearsky77/webview/MainActivity.kt
@@ -2,10 +2,23 @@ package com.clearsky77.webview
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.webkit.WebChromeClient
+import android.webkit.WebViewClient
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        webView.settings.javaScriptEnabled = true // 자바스크립트 허용
+
+        /* 웹뷰에서 새 브라우저로 열지 않도록 */
+        webView.webViewClient = WebViewClient()
+        webView.webChromeClient = WebChromeClient()
+
+        webView.loadUrl("https://www.naver.com")
     }
+
 }
